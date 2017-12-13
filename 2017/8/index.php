b93d30b7fc05c25ec1a73bd5619f0fb92366ffc0
Le-Roux Enslin <le-rouxe@miway.co.za>
2017-12-13 22:54:30 +0200

    Added puzzle input

--- 2017/8/index.php
+++ 2017/8/index.php
@@ -3,4 +3,16 @@
 ini_set('display_errors', '1');
 $input = file("input.txt", FILE_IGNORE_NEW_LINES);
 
+$insRead = new InstructionReader($input);
+
 echo "Part1: ";
+
+class InstructionReader
+{
+    protected $input;
+
+    public function __construct($input)
+    {
+        $this->input = $input;
+    }
+}
