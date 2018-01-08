e8aa16572a531f6a853fd8ae347d90422f714892
Le-Roux Enslin <le-rouxe@miway.co.za>
2018-01-08 15:39:52 +0200

    Day 10, part 1 refactor

--- 2017/10/index.php
+++ 2017/10/index.php
@@ -11,74 +11,54 @@ echo "<br/>Part2: ";
 class Hash
 {
     const LENGTH = 6;
-    protected $string;
+    protected $string = [];
     protected $lengthSequence;
-    protected $currentLength;
-    protected $skipSize;
+    protected $currentNode = 0;
+    protected $skipSize = 0;
 
     public function __construct(array $input)
     {
         $this->lengthSequence = $input;
 
         $this->buildString();
+        $this->processHash();
+
         $this->printString();
     }
 
     private function buildString(): void
     {
         for ($i = 0; $i < self::LENGTH; $i++) {
-            $node = new Node($i);
-
-            $prev = 0 !== $i ? $i-1 : self::LENGTH-1;
-            $next = self::LENGTH !== ($i+1) ? $i+1 : 0;
-
-            $node->setNextNode($next);
-            $node->setPrevNode($prev);
-
-            $this->string[] = $node;
+            $this->string[] = $i;
         }
     }
 
-    private function printString() {
+    private function processHash(): void
+    {
+        foreach ($this->lengthSequence as $span) {
+            $this->twistString((int) $span);
+
+            $this->skipNodes((int) $span);
+        }
+    }
+
+    private function skipNodes(int $span): void
+    {
+        $this->currentNode += $this->skipSize;
+        $this->currentNode += $span;
+
+        ++$this->skipSize;
+    }
+
+    private function twistString(int $param): void
+    {
+
+    }
+
+    private function printString(): void
+    {
         foreach ($this->string as $node) {
             var_dump($node);
         }
-    }
-}
-
-class Node
-{
-    protected $prevNode;
-    protected $value;
-    protected $nextNode;
-
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-
-    public function getValue(): int
-    {
-        return $this->value;
-    }
-
-    public function getPrevNode(): int
-    {
-        return $this->prevNode;
-    }
-
-    public function setPrevNode($prevNode): void
-    {
-        $this->prevNode = $prevNode;
-    }
-
-    public function getNextNode(): int
-    {
-        return $this->nextNode;
-    }
-
-    public function setNextNode($nextNode): void
-    {
-        $this->nextNode = $nextNode;
     }
 }
