f147efd9bd8ec5b589c556c6fdfff6331e57b8bb
Le-Roux Enslin <le-rouxe@miway.co.za>
2018-12-02 18:02:53 +0200

    2018 day 2 part 2

--- 2018/2/index.php
+++ 2018/2/index.php
@@ -3,20 +3,10 @@
 ini_set('display_errors', '1');
 $input = file("input.txt", FILE_IGNORE_NEW_LINES);
 
-$input = [
-    'abcdef',
-    'bababc',
-    'abbcde',
-    'abcccd',
-    'aabcdd',
-    'abcdee',
-    'ababab',
-];
-
 $calc = new ChecksumCalculator($input);
 
 echo "Part1: " . $calc->getBasicChecksum();
-//echo "<br/>Part2: " . $calc->();
+echo "<br/>Part2: " . $calc->getCommonLetters();
 
 class ChecksumCalculator {
     private $input;
@@ -41,11 +31,41 @@ class ChecksumCalculator {
         return $doubles * $triples;
     }
 
+    public function getCommonLetters(): string
+    {
+        $targetLength = strlen($this->input[0]) - 1;
+
+        for ($i = 1; $i < sizeof($this->input); ++$i) {
+            for ($j = $i; $j < sizeof($this->input); ++$j) {
+                $output = $this->getSameCharPositionString($this->input[$i-1], $this->input[$j]);
+
+                if ($targetLength === strlen($output)) {
+                    return $output;
+                }
+            }
+        }
+
+        return 'Not found';
+    }
+
+    private function getSameCharPositionString(string $left, string $right): string
+    {
+        $string = '';
+
+        for ($i = 0; $i < strlen($left); ++$i) {
+            if ($left[$i] === $right[$i]) {
+                $string .= $left[$i];
+            }
+        }
+
+        return $string;
+    }
+
     private function getCharCount(string $string): array
     {
         $chars = [];
 
-        for($i = 0; $i < strlen($string); ++$i) {
+        for ($i = 0; $i < strlen($string); ++$i) {
             isset($chars[$string[$i]]) ? $chars[$string[$i]] += 1 : $chars[$string[$i]] = 1;
         }
 
