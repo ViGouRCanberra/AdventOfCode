84ef8a7b46b0c7d55404e1f23e1006c0fd037c8d
ViGouRCanberra <Canb3rra!>
2019-12-02 22:43:05 +0200

    Added day 2, part 1, 2019

---
+++ 2019/2/index.php
@@ -0,0 +1,58 @@
+<?php
+
+ini_set('display_errors', '1');
+$stars = new Space();
+$input = explode(',', file("input.txt", FILE_IGNORE_NEW_LINES)[0]);
+
+echo "Part1: " . $stars->part1($input) . PHP_EOL;
+echo "Part2: " . $stars->part2($input) . PHP_EOL;
+
+class Space
+{
+    public function part1(array $input): int
+    {
+        $index = 0;
+        $current = $input[0];
+
+        self::restoreProgram($input);
+
+        while ($current != 99) {
+            switch ($current) {
+                case 1:
+                    self::addUp(++$index, $input);
+                    break;
+                case 2:
+                    self::multiply(++$index, $input);
+                    break;
+                default:
+                    throw new \Exception('HOUSTON, WE HAVE A PROBLEM');
+            }
+
+            $index += 3;
+            $current = $input[$index];
+        }
+
+        return $input[0];
+    }
+
+    public function part2(array $input)
+    {
+        return 0;
+    }
+
+    private function restoreProgram(array &$input): void
+    {
+        $input[1] = 12;
+        $input[2] = 2;
+    }
+
+    private function addUp(int $index, array &$input): void
+    {
+        $input[$input[$index + 2]] = $input[$input[$index]] + $input[$input[$index + 1]];
+    }
+
+    private function multiply(int $index, array &$input): void
+    {
+        $input[$input[$index + 2]] = $input[$input[$index]] * $input[$input[$index + 1]];
+    }
+}
